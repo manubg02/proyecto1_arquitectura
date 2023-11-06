9a87f2fb804f7b90236651cd048657a4d13faa62
manubg02 <100451205@alumnos.uc3m.es>
2023-11-06 23:29:26 +0100

    lectura particulas

--- fluid.cpp
+++ fluid.cpp
@@ -7,7 +7,45 @@
 #include <iomanip>
 #include <vector>
 #include <string>
+#include <fstream>
+#include <filesystem>
 
-int main(){
-    return 0;
+int main() {
+    std::ifstream file("../small.fld", std::ios::binary);
+
+    if (file) {
+        // Lee los primeros 4 bytes como un entero de 32 bits
+        int32_t intValue;
+        file.read(reinterpret_cast<char *>(&intValue), sizeof(intValue));
+
+        // Interpreta el entero como un valor de coma flotante de 32 bits
+        float ppm = *reinterpret_cast<float *>(&intValue);
+
+        // Imprime el valor en la consola
+        std::cout << "Particulas por metro: " << ppm << std::endl;
+
+        file.seekg(0, std::ios::cur);
+        file.read(reinterpret_cast<char *>(&intValue), sizeof(intValue));
+        int np = *reinterpret_cast<int *>(&intValue);
+        std::cout << "Numero de particulas: " << np << std::endl;
+
+
+        int count = 0; // Inicializa count antes del bucle
+        while (count < np) {
+            std::cout << "Particula " << count  << ": (";
+            for (int i = 0; i < 8; i++) {
+                file.read(reinterpret_cast<char *>(&intValue), sizeof(intValue));
+                float particula = *reinterpret_cast<float *>(&intValue);
+                std::cout << std::fixed << std::setprecision(8) << particula << ", ";
+
+
+            }
+            file.read(reinterpret_cast<char *>(&intValue), sizeof(intValue));
+            float lastParticula = *reinterpret_cast<float *>(&intValue);
+            std::cout << std::fixed << std::setprecision(8) << lastParticula << ")" << std::endl;
+            count += 1;
+        }
+
+        return 0;
+    }
 }
